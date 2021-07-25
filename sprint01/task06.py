ecfdbc20c248d532d53fab7efb7f011b51247778
Taras-Vasevych <73836875+Taras-Vasevych@users.noreply.github.com>
2021-07-25 19:57:47 +0300

    Добавленно более правильное решение задачи

--- sprint01/task06.py
+++ sprint01/task06.py
@@ -5,3 +5,20 @@ def order(a):
     if a == b[::-1]:
         return "descending"
     return "not sorted"
+
+
+# Более удачное решение с меньшей сложностью
+def order(a):
+    for present_element, next_element in zip(a, a[1:]):
+        if not next_element >= present_element:
+            break
+    else:
+        return "ascending"
+    
+    for present_element, next_element in zip(a, a[1:]):
+        if not next_element <= present_element:
+            break
+    else:
+        return "descending"
+
+    return 'not sorted'
