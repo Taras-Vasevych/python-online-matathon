40e93eef7d33e378dcd6e0fb725c01c5ce135da2
Taras-Vasevych <73836875+Taras-Vasevych@users.noreply.github.com>
2021-07-26 14:53:00 +0300

    Create task01.py

---
+++ sprint02/task01.py
@@ -0,0 +1,15 @@
+import re
+
+def max_population(data):
+    sep = ','
+    pattern = re.compile(
+        sep.join(f'(?P<{name}>[^{sep}]*)'
+            for name in data[0].split(sep)                 
+        )
+    )
+    m_gen = (
+        pattern.fullmatch(line)
+        for line in data[1:]
+    )
+    biggest_city = max(m_gen, key=lambda m: m.group('poppulation'))
+    return biggest_city.group('name'), biggest_city.group('poppulation')
