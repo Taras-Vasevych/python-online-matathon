714cd6f214cd60dcfa452647524af9247695ccb1
Taras-Vasevych <73836875+Taras-Vasevych@users.noreply.github.com>
2021-07-27 21:37:56 +0300

    Another sollution

--- sprint02/task02a.py
+++ sprint02/task02a.py
@@ -1,10 +1,6 @@
 def digit_to_morse(digit):
-    n = int(digit)
-    return(
-        '-----'.replace('-', '.', n)
-        if n <= 5 else
-        '.....'.replace('.', '-', n-5)
-    )
+    n = 9 - int(digit)
+    return '----.....-----'[n:n+5]
 
 
 def morse_number(number):
