a7a40d20d2644713a8eb5a38ffd1164aa400d874
Taras-Vasevych <73836875+Taras-Vasevych@users.noreply.github.com>
2021-08-02 18:09:33 +0300

    Create task05.py

---
+++ sprint03/task05.py
@@ -0,0 +1,23 @@
+from itertools import chain
+
+
+def logger(fn):
+    def inner(*args, **kwargs):
+        fn(*args, **kwargs)
+        name = fn.__name__
+        arguments = ', '.join(str(x) for x in chain(args, kwargs.values()))
+        print(f'Executing of function {name} with arguments {arguments}...')
+        #return fn(*args, **kwargs)
+    return inner
+
+@logger
+def concat(*args, **kwargs):
+    return ''.join(str(x) for x in chain(args, kwargs.values()))
+
+@logger
+def sum(a,b):
+    return a+b
+    
+@logger
+def print_arg(arg):
+    print(arg)
